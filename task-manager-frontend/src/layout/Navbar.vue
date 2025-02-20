9d3d9e3049d997a1e59aef1a525b632641dec685
jatnipit <jatnipt.k@ku.th>
2025-02-20 10:54:14 +0700

    Login Page updates

--- task-manager-frontend/src/layout/Navbar.vue
+++ task-manager-frontend/src/layout/Navbar.vue
@@ -5,14 +5,57 @@ const router = useRouter();
 </script>
 
 <template>
-  <div class="navbar bg-base-100 shadow-lg">
-    <div class="navbar-start">
-      <div class="drawer">
-        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content">
-          <!-- Page content here -->
-          <label for="my-drawer" class="btn btn-ghost btn-circle drawer-button"
-            ><svg
+  <div class="fixed left-0 top-0 z-[999] w-full bg-base-100 shadow-lg">
+    <div class="container navbar mx-auto">
+      <div class="navbar-start">
+        <div class="drawer">
+          <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+          <div class="drawer-content">
+            <!-- Page content here -->
+            <label
+              for="my-drawer"
+              class="btn btn-ghost btn-circle drawer-button"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+          </div>
+          <div class="drawer-side">
+            <label
+              for="my-drawer"
+              aria-label="close sidebar"
+              class="drawer-overlay"
+            ></label>
+            <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+              <!-- Sidebar content here -->
+              <li><a>Homepage</a></li>
+              <li><a>Portfolio</a></li>
+              <li><a>About</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <!-- <a class="btn btn-ghost text-xl"></a> -->
+        <RouterLink to="/" class="btn btn-ghost text-xl"
+          >Task Manager</RouterLink
+        >
+      </div>
+      <div class="navbar-end">
+        <button class="btn btn-ghost btn-circle">
+          <div class="indicator">
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-5 w-5"
               fill="none"
@@ -23,74 +66,41 @@ const router = useRouter();
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h7"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />
             </svg>
-          </label>
-        </div>
-        <div class="drawer-side">
-          <label
-            for="my-drawer"
-            aria-label="close sidebar"
-            class="drawer-overlay"
-          ></label>
-          <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            <!-- Sidebar content here -->
-            <li><a>Homepage</a></li>
-            <li><a>Portfolio</a></li>
-            <li><a>About</a></li>
+            <span class="badge badge-xs badge-primary indicator-item"></span>
+          </div>
+        </button>
+        <div class="dropdown dropdown-end">
+          <div
+            tabindex="0"
+            role="button"
+            class="btn btn-ghost btn-circle avatar"
+          >
+            <div class="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li>
+              <RouterLink to="/login">Login</RouterLink>
+            </li>
           </ul>
         </div>
-      </div>
-    </div>
-    <div class="navbar-center">
-      <!-- <a class="btn btn-ghost text-xl"></a> -->
-      <RouterLink to="/" class="btn btn-ghost text-xl">Task Manager</RouterLink>
-    </div>
-    <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            />
-          </svg>
-          <span class="badge badge-xs badge-primary indicator-item"></span>
-        </div>
-      </button>
-      <div class="dropdown dropdown-end">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-            />
-          </div>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-        >
-          <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
-          </li>
-          <li><a>Settings</a></li>
-          <li>
-            <RouterLink to="/login">Login</RouterLink>
-          </li>
-        </ul>
       </div>
     </div>
   </div>
--- task-manager-frontend/src/views/LoginView.vue
+++ task-manager-frontend/src/views/LoginView.vue
@@ -1,3 +1,53 @@
+<script setup>
+import { RouterLink, useRouter } from "vue-router";
+
+const router = useRouter();
+
+const formData = reactive({
+  username: "",
+  password: "",
+});
+</script>
+
 <template>
-  <h1>Login Page</h1>
+  <div class="flex items-center justify-center min-h-screen bg-base-200">
+    <div class="w-full max-w-sm p-8 space-y-4 bg-white shadow-lg rounded-lg">
+      <h2 class="text-2xl font-bold text-center">Login</h2>
+      <form @submit.prevent="handleLogin">
+        <div class="form-control">
+          <label class="label" for="email">
+            <span class="label-text">Email or Username</span>
+          </label>
+          <input
+            id="email"
+            type="email"
+            v-model="formData.username"
+            placeholder="Enter your email or username"
+            class="input input-bordered w-full"
+            required
+          />
+        </div>
+        <div class="form-control">
+          <label class="label" for="password">
+            <span class="label-text">Password</span>
+          </label>
+          <input
+            id="password"
+            type="password"
+            v-model="formData.password"
+            placeholder="Enter your password"
+            class="input input-bordered w-full"
+            required
+          />
+        </div>
+        <div class="form-control mt-6">
+          <button class="btn btn-primary w-full" type="submit">Login</button>
+        </div>
+      </form>
+      <p class="text-center">
+        Don't have an account?
+        <RouterLink to="/register" class="text-primary">Sign up</RouterLink>
+      </p>
+    </div>
+  </div>
 </template>
